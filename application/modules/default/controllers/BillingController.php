8312ba37788bdd4e8a9153c592a32ad4a539c2aa
Amit Periyapatna <amperiya@cisco.com>
2015-04-13 18:20:39 +0530

    Adding all source files

---
+++ application/modules/default/controllers/BillingController.php
@@ -0,0 +1,12 @@
+<?php
+
+class Default_BillingController extends Zend_Controller_Action
+{
+	public function init(){
+
+	}	
+	public function indexAction(){
+		/*Default index action*/
+		$this->view->headTitle("Nandan Selections - Billing");
+	}
+}
---
+++ application/modules/default/controllers/CustomerController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Default_CustomerController extends Zend_Controller_Action
+{
+	public function init(){
+
+	}
+
+	public function indexAction(){
+
+	}
+	public function addAction(){
+		$this->view->headTitle("Nandan Selections - Add Customer");
+	}
+}
