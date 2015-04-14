9498f16e2f5a0d867277a9941cbf2963eb308fe2
Amit Periyapatna <amperiya@cisco.com>
2015-04-14 19:49:46 +0530

    Added backend functionality and frontend

--- application/modules/default/controllers/BillingController.php
+++ application/modules/default/controllers/BillingController.php
@@ -8,5 +8,53 @@ class Default_BillingController extends Zend_Controller_Action
 	public function indexAction(){
 		/*Default index action*/
 		$this->view->headTitle("Nandan Selections - Billing");
+
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$select = $db->select()
+					 ->from("transaction_type");
+		try{
+			$result = $db->query($select)->fetchAll();
+		}
+		catch(Exception $e){
+
+		}
+		$transaction_type = "<option value = 0 selected>Select a Transaction Type</option>";
+		foreach($result as $e){
+			$transaction_type .= "<option value = ".$e['id'].">".$e['desc']."</option>";
+		}
+		$this->view->select = $transaction_type;
+	}
+	public function postAction(){
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$params = $this->_getAllParams();
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$data = array(
+			"desc" => $params['desc'],
+			"transaction_type" => $params['transaction_type'],
+			"amount" => $params['amount'],
+			"customer_id" => $params['customer_id'],
+			"date" => $params['date']
+		);
+		try{
+			$db->insert("transactions",$data);
+			if($params['transaction_type'] == 1)
+				$update_string = "balance - " . $params['amount'];
+			else if($params['transaction_type'] == 2)
+				$update_string = "balance + " . $params['amount'];
+			else 
+				return;
+			$update_data = array(
+				"balance" => new Zend_Db_Expr($update_string)
+			);
+			$where = array(
+				"customer_id = ?" => $params['customer_id']
+			);
+			$db->update("balance",$update_data,$where);
+		}
+		catch(Exception $e){
+			print_r($e);
+		}
+		echo "Successful";
 	}
 }
--- application/modules/default/controllers/CustomerController.php
+++ application/modules/default/controllers/CustomerController.php
@@ -12,4 +12,61 @@ class Default_CustomerController extends Zend_Controller_Action
 	public function addAction(){
 		$this->view->headTitle("Nandan Selections - Add Customer");
 	}
+	public function postAction(){
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$params = $this->_getAllParams();
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$data = array(
+			"name" => $params['name'],
+			"contact_no" => $params['contact_no'],
+			"address" => $params['address']
+			);
+		try{
+			$db->insert("customers",$data);
+		}
+		catch(Exception $e){
+			switch(get_class($exception)){
+				case 'Zend_Argument_Exception': $message = 'Argument Error.'; break;
+				case 'Zend_Db_Statment_Exception': $message = 'Database Error.'; break;
+				case 'default': $message = "Unknown Error."; break;
+			}
+		}
+		$last_insert_id = $db->lastInsertId();
+		$balance = $params['balance'];
+		$data = array(
+			"customer_id" => $last_insert_id,
+			"balance" => $balance
+		);
+		try{
+			$db->insert("balance",$data);
+		}
+		catch(Exception $e){
+			switch(get_class($exception)){
+				case 'Zend_Argument_Exception': $message = 'Argument Error.'; break;
+				case 'Zend_Db_Statment_Exception': $message = 'Database Error.'; break;
+				case 'default': $message = "Unknown Error."; break;
+			}
+		}
+		echo "Customer successfully created!";
+	}
+	public function getAllAction(){
+		header('Content-Type: application/json');
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$select = $db->select()
+					 ->from("customers");
+		try{
+			$result = $db->query($select)->fetchAll();
+		}
+		catch(Exception $e){
+
+		}
+		echo json_encode($result);
+	}
+	public function viewAction(){
+		$this->view->headTitle("Nandan Selections - View Customer Details");
+		
+	}
 }
