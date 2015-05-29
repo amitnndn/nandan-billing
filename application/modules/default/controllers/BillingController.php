8a328fcb5fffc634a313f80914f4e8959f8a05b2
Amit Periyapatna <amperiya@cisco.com>
2015-05-29 17:32:24 +0530

    Imporving view styles

--- application/modules/default/controllers/BillingController.php
+++ application/modules/default/controllers/BillingController.php
@@ -29,15 +29,7 @@ class Default_BillingController extends Zend_Controller_Action
 		$this->_helper->viewRenderer->setNoRender(TRUE);
 		$params = $this->_getAllParams();
 		$db = Zend_Db_Table::getDefaultAdapter();
-		$data = array(
-			"desc" => $params['desc'],
-			"transaction_type" => $params['transaction_type'],
-			"amount" => $params['amount'],
-			"customer_id" => $params['customer_id'],
-			"date" => $params['date']
-		);
 		try{
-			$db->insert("transactions",$data);
 			if($params['transaction_type'] == 1)
 				$update_string = "balance - " . $params['amount'];
 			else if($params['transaction_type'] == 2)
@@ -51,10 +43,53 @@ class Default_BillingController extends Zend_Controller_Action
 				"customer_id = ?" => $params['customer_id']
 			);
 			$db->update("balance",$update_data,$where);
+			$select = $db->select()
+						 ->from("balance")
+						 ->where("customer_id = ".$params['customer_id']);
+			$balance = $db->query($select)->fetchAll();
+			$bal = $balance[0]["balance"];
+		}
+		catch(Exception $e){
+			print_r($e);
+		}
+		$data = array(
+			"desc" => $params['desc'],
+			"transaction_type" => $params['transaction_type'],
+			"amount" => $params['amount'],
+			"customer_id" => $params['customer_id'],
+			"date" => $params['date'],
+			"balance" => $bal
+		);
+		try{
+			$db->insert("transactions",$data);
 		}
 		catch(Exception $e){
 			print_r($e);
 		}
 		echo "Successful";
+	}
+	public function testAction(){
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$params = $this->_getAllParams();
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$select = $db->select()
+						 ->from("balance")
+						 ->where("customer_id = ".$params['customer_id']);
+		$balance = $db->query($select)->fetchAll();
+		$bal = $balance[0]["balance"];
+		print_r($bal);
+	}
+	public function getBalanceAction(){
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$params = $this->_getAllParams();
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$customer_id = $params['id'];
+		$select = $db->select()
+					 ->from("balance")
+					 ->where("customer_id = $customer_id");
+		$result = $db->query($select)->fetchAll();
+		echo $result[0]["balance"];
 	}
 }
--- application/modules/default/controllers/CustomerController.php
+++ application/modules/default/controllers/CustomerController.php
@@ -26,11 +26,13 @@ class Default_CustomerController extends Zend_Controller_Action
 			$db->insert("customers",$data);
 		}
 		catch(Exception $e){
-			switch(get_class($exception)){
+			switch(get_class($e)){
 				case 'Zend_Argument_Exception': $message = 'Argument Error.'; break;
 				case 'Zend_Db_Statment_Exception': $message = 'Database Error.'; break;
 				case 'default': $message = "Unknown Error."; break;
 			}
+			print_r($e);
+			exit;
 		}
 		$last_insert_id = $db->lastInsertId();
 		$balance = $params['balance'];
@@ -38,17 +40,29 @@ class Default_CustomerController extends Zend_Controller_Action
 			"customer_id" => $last_insert_id,
 			"balance" => $balance
 		);
+		$date = date("Y-m-d");
+		$trans_data = array(
+			"desc" => "Initial Balance",
+			"date" => $date,
+			"customer_id" => $last_insert_id,
+			"transaction_type" => 2,
+			"amount" => $balance,
+			"balance" => $balance
+		);
 		try{
 			$db->insert("balance",$data);
+			$db->insert("transactions",$trans_data);
 		}
 		catch(Exception $e){
-			switch(get_class($exception)){
+			switch(get_class($e)){
 				case 'Zend_Argument_Exception': $message = 'Argument Error.'; break;
 				case 'Zend_Db_Statment_Exception': $message = 'Database Error.'; break;
 				case 'default': $message = "Unknown Error."; break;
 			}
+			print_r($e);
+			exit;
 		}
-		echo "Customer successfully created!";
+		echo "TRUE";
 	}
 	public function getAllAction(){
 		header('Content-Type: application/json');
@@ -67,6 +81,46 @@ class Default_CustomerController extends Zend_Controller_Action
 	}
 	public function viewAction(){
 		$this->view->headTitle("Nandan Selections - View Customer Details");
+
+	}
+	public function testAction(){
+		header('Content-Type: application/json');
+		$data = $this->_getAllParams();
+		$customer_id = $data['id'];
 		
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(TRUE);
+		$db = Zend_Db_Table::getDefaultAdapter();
+		$select = $db->select()
+					 ->from("transactions")
+					 ->where("customer_id = $customer_id");
+		$result = $db->query($select)->fetchAll();
+		$select1 = $db->select()
+					  ->from("balance")
+					  ->where("customer_id = $customer_id");
+		$balance_array = $db->query($select1)->fetchAll();
+		$balance = $balance_array[0]['balance'];
+		$response = array(
+			"data" => "",
+			"balance"=>"$balance"
+			);	
+		$array = array();
+		foreach($result as $a){
+			$a['action'] = "<a href='/billing/edit?id=".$a['id'].".'>Edit</a>&nbsp;|&nbsp;<a href='/billing/delete?id=".$a['id']."'>Delete</a>";
+			$a['balance'] = "Rs. ".$a['balance'];
+			if($a['transaction_type'] == 1){
+				$a['transaction_type'] = "Debit";
+				$amount = $a['amount'];
+				$a['amount'] = "<span style='color: green;'> + Rs. " . $a['amount'];
+			}
+			else if($a['transaction_type'] == 2){
+				$a['transaction_type'] = "Credit";
+				$amount = $a['amount'];
+				$a['amount'] = "<span style='color: red;'> - Rs. " . $a['amount'];
+			}
+			array_push($array,$a);
+		}
+		$response["data"] = $array;
+		echo json_encode($response);
 	}
 }
